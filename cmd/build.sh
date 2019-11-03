7f57e1ed2fb1a13da971e8e1f0f92dc3ff26a0ac
Bradley Gannon <bradley@bradleygannon.com>
2019-11-03 15:39:38 -0500

    Make editor narrower

--- cmd/build.sh
+++ cmd/build.sh
@@ -8,7 +8,9 @@ user_auth.go
 # build and arrange EasyMDE if it exists
 if [ -d ../easy-markdown-editor ]; then
     cd ../easy-markdown-editor
-    cp ../editor-src/js/easymde.js ./src/js/easymde.js
+    cp -r ../editor-src/js/easymde.js ./src/js/easymde.js
+    cp -r ../editor-src/css/easymde.css ./src/css/easymde.css
     gulp
-    cp ./dist/easymde.min.js ../cmd/static/js/easymde.min.js
+    cp -r ./dist/easymde.min.js ../cmd/static/js/easymde.min.js
+    cp -r ./dist/easymde.min.css ../cmd/static/css/easymde.min.css
 fi

531de765865b18cae586a2e8d68ce1a2b6eb1ebf
Bradley Gannon <bradley@bradleygannon.com>
2019-11-16 19:46:30 -0500

    Fix .gitignore typo and improve build script
    There was a typo in .gitignore that didn't properly ignore
    easymde.min.js. That's fixed now.
    
    Since the minified files are ignored, the static/ directory isn't
    currently being tracked. In the future, this will probably change as we
    add more static files, but for now there's a possibility that the local
    copy of the repo won't have a static/ tree. This can cause the build
    script to fail because there's no place for the script to put the
    minified CSS and JS files. The build script now accounts for this case.

--- cmd/build.sh
+++ cmd/build.sh
@@ -7,9 +7,20 @@ user_auth.go
 
 # build and arrange EasyMDE if it exists
 if [ -d ../easy-markdown-editor ]; then
+    # if there aren't any static folders, then make them
+    if [ ! -d ../cmd/static/js ]; then
+        mkdir -p ../cmd/static/js
+    fi
+    if [ ! -d ../cmd/static/css ]; then
+        mkdir -p ../cmd/static/css
+    fi
+
+    # copy the source files into the EasyMDE repo
     cd ../easy-markdown-editor
     cp -r ../editor-src/js/easymde.js ./src/js/easymde.js
     cp -r ../editor-src/css/easymde.css ./src/css/easymde.css
+
+    # build EasyMDE and copy the minified files to the right places
     gulp
     cp -r ./dist/easymde.min.js ../cmd/static/js/easymde.min.js
     cp -r ./dist/easymde.min.css ../cmd/static/css/easymde.min.css
